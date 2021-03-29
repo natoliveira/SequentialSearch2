1f70ecf88c1be92f16e9244747fa44d1ee4a40d7
natoliveira <nataliadeolive@gmail.com>
2021-03-28 23:12:27 -0300

    inserindo algoritmo sequentialSearch2

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int b;
+    int c;
+
+
+    int i = 0;
+    cout << "b\c";
+    cin >> c;
+    int a[n];
+
+    cout << "Entre com os " << c << "\c";
+    for (i = 0; i < c; i++) {
+        cin >> a[i];
+    }
+    cout << "\c";
+    cin >> b;
+
+
+
+
+         a[c]=k;
+
+         i=0;
+
+
+
+         while (a[i] != k)
+                 i = i + 1;
+            if (i < n) {
+
+                cout <<"A posição do item é : " << i;
+
+                return i;
+
+
+            } else{
+				cout <<"O item não existe na matrix:\c";
+                cout << -1;
+                return -1;
+            }
+
+
+
+}
